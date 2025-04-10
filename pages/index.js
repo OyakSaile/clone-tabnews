7a6320404d2799240ca58c2ee6f05ae047b99e37
Kayo Elias <46985165+OyakSaile@users.noreply.github.com>
2025-04-10 02:43:22 +0000

    frase motivacional =)

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
 function Home() {
-    return <h1>Kayo Elias</h1>
+    return <div>
+        <h1>Olá, Tudo bem?</h1>
+        <p>Aqui vai uma dica, estou a um tempo na área, no começo foi dificíl, não tinha um curso com essa qualidade, mas agora estamos diante de algo incrível, que massa né?</p>
+        <br />
+        <p>Minha dica é, não desista você vai conseguir, eu mudei minha vida devido a programação, então você vai conseguir, caso precise de alguma ajuda pode contar comigo.</p>
+
+
+        <div>
+            <p>Me encontre aqui:</p>
+            <a target="_blank" href="https://www.linkedin.com/in/kayoeliasgverdan/">Linkedin</a>
+        </div>
+    </div>
 }
 
 
