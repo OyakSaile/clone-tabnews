5c4ef308b8745ae024507f2d5c640da2cb311efd
Kayo Elias <46985165+OyakSaile@users.noreply.github.com>
2025-04-24 20:15:00 +0000

    database mais robusto ao lidar com erros

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,12 +1,34 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum");
+  const updatedAt = new Date().toISOString();
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  const databaseMaxConnectionResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionValue =
+    databaseMaxConnectionResult.rows[0].max_connections;
 
-  console.log(result.rows);
+  const databaseName = process.env.POSTGRES_DB;
+
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseOpenedConnectionValue =
+    databaseOpenedConnectionsResult.rows[0].count;
 
   response.status(200).json({
-    chave: "são acima da média",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionValue),
+        opened_connections: databaseOpenedConnectionValue,
+      },
+    },
   });
 }
 
